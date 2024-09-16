852638df8f9be9f5b32b273a021748935a34752b
Lara-P-Santos <lara.santos4@estudante.ifms.edu.br>
2024-09-16 09:50:41 -0400

    Adicionando css nas páginas de cadastro e login

--- TrabalhoFinalDW2/V/cadastro.php
+++ TrabalhoFinalDW2/V/cadastro.php
@@ -4,28 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
-    <style>
-    input,form{
-        margin: 5px;
-        flex-direction: row;
-    }
-    </style>
+    <link rel="stylesheet" href="cadastro.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />                    
 </head>
 <body>
 
-<h2>Cadastro de trabalhos</h2>
-
 <form action="../C/trabalho.php" method="POST">
+    
+    <span class="material-symbols-outlined">add_task</span>
+    <h2>Cadastro de trabalhos</h2>
 
-    <input type="text" placeholder="Título" name="titulo" required maxlength="128">
-    <input type="text" placeholder="Descreva aqui o seu trabalho:" required maxlength="1000">
-    <select name="categorias">
-        <option value=" "> </option>
+    <input type="text" class="titulo" placeholder="Título" name="titulo" required maxlength="128">
+    <textarea class="desc" placeholder="Descreva aqui o seu trabalho:" required maxlength="1000"></textarea><br>
+    <select name="categorias">Categoria:
+        <option value="select">Selecionar categoria</option>
         <option value="exatas">Ciências Exatas</option>
         <option value="humanas">Ciências Humanas</option>
         <option value="biologicas">Ciências Biológicas</option>
-    </select>
-    <input type="submit" value="Enviar">
+    </select><br>
+    <input type="submit" value="Cadastrar Trabalho" class="enviar">
     
 </form>
 
--- TrabalhoFinalDW2/V/login.php
+++ TrabalhoFinalDW2/V/login.php
@@ -3,22 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./login.css">
     <title>Login</title>
-    <style>
-        input,form{
-            margin: 5px;
-            flex-direction: row;
-        }
-    </style>
+    <link rel="shortcut icon" href="./icon.png">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />                    
 </head>
 <body>
-    <h1>Login</h1>
 
     <form action="../C/login.php" method="POST">
+        
+        <span class="material-symbols-outlined">lock</span>
+        <h1>Login</h1>
 
         <input type="text" placeholder="Nome" name="nome" required>
         <input type="password" placeholder="Senha" name="senha" required>
-        <input type="submit" name="Enviar">
+        <input type="submit" value="Enviar">
 
     </form>
 
